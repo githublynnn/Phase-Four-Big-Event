d37d778680220b2f0276b2d1e5311c62f9af0bbf
lyn <liuynn00@163.com>
2022-09-16 17:16:32 +0800

    '完成了后台首页的开发'

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,22 @@
 $.ajaxPrefilter(function (options) {
+  // 拼接根路径
   options.url = 'http://www.liulongbin.top:3007' + options.url
+
+  // 有权限的页面携带请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || '',
+    }
+  }
+  //complete回调函数
+  options.complete = function (res) {
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === '身份认证失败！'
+    ) {
+      // console.log(1)
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+$(function () {
+  // 只要进入到首页，就调用获取用户基本信息的函数
+  getUserInfo()
+
+  $('#btnLogout').click(function () {
+    layer.confirm(
+      '确定退出登录?',
+      { icon: 3, title: '提示' },
+      function (index) {
+        //do something
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+        layer.close(index)
+      }
+    )
+  })
+})
+
+// 获取用户的基本信息
+function getUserInfo() {
+  $.ajax({
+    url: '/my/userinfo',
+    method: 'GET',
+    // headers: {
+    //   Authorization: localStorage.getItem('token') || '',
+    // },
+    success: function (res) {
+      if (res.status !== 0) {
+        return layui.layer.msg('获取用户信息失败！')
+      }
+      // 成功获取用户信息后，就调用用户信息数据开始渲染函数
+      // console.log(res.data)
+      renderAvatar(res.data)
+    },
+
+    // complete: function (res) {
+    //   // console.log(res)
+    //   if (
+    //     res.responseJSON.status === 1 &&
+    //     res.responseJSON.message === '身份认证失败！'
+    //   ) {
+    //     // console.log(1)
+    //     localStorage.removeItem('token')
+    //     location.href = '/login.html'
+    //   }
+    // },
+  })
+}
+
+function renderAvatar(user) {
+  const name = user.nickname || user.username
+  $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+  if (user.user_pic !== null) {
+    $('.layui-nav-img').arrt('src', user.user_pic).show()
+    $('.text-avatar').hide()
+  } else {
+    const first = name[0].toUpperCase()
+    $('.text-avatar').html(first).show()
+    $('.layui-nav-img').hide()
+  }
+}
